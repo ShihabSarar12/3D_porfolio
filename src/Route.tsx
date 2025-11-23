8c100fe16396b23913a1ee75b28ceaae98574755
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-11-23 12:46:03 +0600

    projects sections rebuild complete

--- src/Route.tsx
+++ src/Route.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Research from "./components/Research";
 import AchieveDetails from "./components/AchieveDetails";
+import ProjectDetails from "./components/ProjectDetails";
 
 export default function RouteWrapper() {
   return (
@@ -11,6 +12,8 @@ export default function RouteWrapper() {
 
         <Route path="/research/:id" element={<Research />} />
         <Route path="/achievement/:id" element={<AchieveDetails />} />
+        <Route path="/achievement/:id" element={<AchieveDetails />} />
+        <Route path="/project/:id" element={<ProjectDetails />} />
       </Routes>
     </BrowserRouter>
   );
